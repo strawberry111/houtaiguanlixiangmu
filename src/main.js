84c9d47fb5b8f736313b5931fffa6fc4f36e27d5
zlj <1018970619@qq.com>
2018-02-27 16:50:47 +0800

    暂存测试

--- src/main.js
+++ src/main.js
@@ -1,2 +1,9 @@
-// 嘻嘻嘻
-的撒旦是
+import Vue from 'vue'
+
+import App from './component/App.vue'
+
+new Vue({
+    el: '#app',
+    // 下面使用的箭头函数也是es6语法
+    render: createElement => createElement(App)
+})
